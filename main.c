e202799a2730ee6921d6d31dbbc88a0c86e0afee
Guilherm772 <guilhermlima772@gmail.com>
2024-11-18 20:01:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void preenche_histrograma(char *h[], int valor, int categoria)
+{
+    for (int i = 0; i < valor; i++)
+    {
+        h[categoria][i] = '*';
+    }
+}
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-    
+void imprimir_histograma(char *h[], int valores[], int categorias)
+{
+    for (int i = 0; i < categorias; i++)
+    {
+        printf("%d ", valores[i]);
+
+        for (int j = 0; j < valores[i]; j++)
+        {
+            printf("%c", h[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+
+    int categorias;
+    scanf("%d", &categorias);
+
+    int valores[categorias];
+    char *histrograma[categorias];
+
+    for (int i = 0; i < categorias; i++)
+    {
+        scanf("%d", &valores[i]);
+        if (valores[i] == 0)
+        {
+            histrograma[i] = NULL;
+        }
+        else
+        {
+            histrograma[i] = (char *)malloc(valores[i] * sizeof(char));
+            preenche_histrograma(histrograma, valores[i], i);
+        }
+    }
+
+    imprimir_histograma(histrograma, valores, categorias);
     return 0;
 }
